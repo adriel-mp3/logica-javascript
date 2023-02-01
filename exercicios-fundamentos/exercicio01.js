4ee09a53da1736163d238b6c30a0afca6bd9d285
Adriel Santos <109179771+adriel-mp3@users.noreply.github.com>
2023-02-01 15:50:57 -0300

    exercise done

--- exercicios-fundamentos/exercicio01.js
+++ exercicios-fundamentos/exercicio01.js
@@ -1,3 +1,8 @@
 // Crie uma função que retorna a string "Olá, " concatenada com um argumento text 
 // (a ser passado para a função) e com ponto de exclamação "!" no final.
+
+function handleString(text) {
+    return `Olá ${text} !`
+}
+console.log(handleString('olá'))
      
